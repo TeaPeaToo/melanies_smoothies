727fda571d9464f1e1f4722f0adac915b6d53cad
TeaPeaToo <tanjaa.pettersson@virginmedia.ie>
2025-06-19 16:31:04 +0100

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-
 # Write directly to the app
 st.title(":cup_with_straw: Customize Yor Smoothie' :cup_with_straw:")
 st.write(
